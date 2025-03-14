b829cc46b84aeb19881b916202c4f71c084e4ed3
malory171 <malory171@gmail.com>
2025-03-15 00:02:06 +0100

    Added footer including links & applied CSS styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,34 @@ function App() {
       <main>
         <Dictionary />
       </main>
-      <footer />
+      <footer className="m-2 text-center">
+        <p>
+          Coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/135692-malory-mezierre"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Miss Malory
+          </a>
+          📖, open-sourced on{" "}
+          <a
+            href="https://github.com/malory171/miss-malory-dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://missmalory-dictionary.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
