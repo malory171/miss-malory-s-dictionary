7bf3b8f361d86c2fc78a4620494f5332588864da
malory171 <malory171@gmail.com>
2025-03-25 17:26:20 +0100

    Improved styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,10 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <section>
-        <h3>{props.meaning.partOfSpeech}</h3>
+        <h3>
+          {props.meaning.partOfSpeech.charAt(0).toUpperCase() +
+            props.meaning.partOfSpeech.slice(1)}
+        </h3>
         {props.meaning.definitions.map(function (definition, index) {
           return (
             <div key={index}>
